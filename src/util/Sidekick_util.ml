0326c07c16c7f42cb4964d5513ccc31f3bd956f2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-02-08 19:44:59 -0600

    refactor: add `Sidekick_util` with some re-exports

---
+++ src/util/Sidekick_util.ml
@@ -0,0 +1,11 @@
+
+(* re-exports *)
+module Fmt = CCFormat
+module Vec = Msat.Vec
+module Log = Msat.Log
+
+module Util = Util
+module Backtrack_stack = Backtrack_stack
+module Error = Error
+module IArray = IArray
+module Intf = Intf
