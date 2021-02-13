fb52e792879eaf599c1f6731c9d639980a37b615
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-02-12 19:41:34 -0500

    test: update test harness

--- src/arith/tests/run_tests.ml
+++ src/arith/tests/run_tests.ml
@@ -1,10 +1,22 @@
 
+let tests : unit Alcotest.test list = [
+  Test_simplex2.tests;
+]
+
 let props =
   List.flatten
-    [ Test_simplex.props;
+    [ Test_simplex2.props;
+      Test_simplex.props;
     ]
 
 let () =
-  CCFormat.set_color_default true;
-  QCheck_runner.run_tests_main props
-
+  match Array.to_list Sys.argv with
+  | a0::"alcotest"::tl ->
+    Sidekick_util.Log.set_debug 50;
+    let argv= Array.of_list (a0::tl) in
+    Alcotest.run ~argv ~and_exit:true "arith tests" tests;
+  | a0::"qcheck"::tl ->
+    let argv= Array.of_list (a0::tl) in
+    CCFormat.set_color_default true;
+    QCheck_runner.run_tests_main ~argv props
+  | _ -> failwith "expected (qcheck|alcotest) as first arg"
