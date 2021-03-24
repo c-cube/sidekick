481e8e9e58f481b14198a8280986677bb9a41ef4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-03-24 12:07:11 -0400

    feat(msat-solver): profiling probe for conflicts

--- src/msat-solver/Sidekick_msat_solver.ml
+++ src/msat-solver/Sidekick_msat_solver.ml
@@ -674,7 +674,12 @@ module Make(A : ARG)
       List.iter (fun f->f()) on_exit;
     in
     self.si.on_progress <- (fun () -> on_progress self);
-    let r = Sat_solver.solve ~assumptions (solver self) in
+    let on_conflict =
+      if Profile.enabled()
+      then Some (fun _ -> Profile.instant "sat.conflict")
+      else None
+    in
+    let r = Sat_solver.solve ?on_conflict ~assumptions (solver self) in
     Stat.incr self.count_solve;
     match r with
     | Sat_solver.Sat st ->
