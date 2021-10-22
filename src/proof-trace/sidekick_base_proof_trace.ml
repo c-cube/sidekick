254d6a1906bd52de7a7cdb08c4de1148908fa80e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-10-21 20:32:34 -0400

    feat: library for the proof step serialization

---
+++ src/proof-trace/sidekick_base_proof_trace.ml
@@ -0,0 +1,8 @@
+
+(** Proof trace with serialization
+
+    This library is useful to serialize a series of reasoning steps
+    in memory or into a file, to be able to reconstruct a proper
+    proof later. *)
+
+module Proof_ser = Proof_ser
