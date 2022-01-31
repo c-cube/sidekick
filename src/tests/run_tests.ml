8b78057058a945a48e24337ff278e908efcf0238
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-01-31 11:08:46 -0500

    test: detail

--- src/tests/run_tests.ml
+++ src/tests/run_tests.ml
@@ -16,13 +16,13 @@ let props =
     ]
 
 let () =
+  (*Sidekick_util.Log.set_debug 50;*)
+  Sidekick_util.Log.set_debug 10;
   match Array.to_list Sys.argv with
   | a0::"alcotest"::tl ->
-    Sidekick_util.Log.set_debug 50;
     let argv= Array.of_list (a0::tl) in
     Alcotest.run ~argv ~and_exit:true "arith tests" tests;
   | a0::"qcheck"::tl ->
-    Sidekick_util.Log.set_debug 50;
     let argv= Array.of_list (a0::tl) in
     CCFormat.set_color_default true;
     QCheck_runner.run_tests_main ~argv props
