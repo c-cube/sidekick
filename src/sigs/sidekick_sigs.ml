ea752b5cf55eb61e66179efde73c972552e673ff
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-07-17 20:21:22 -0400

    feat: add some BACKTRACKABLE sigs

--- src/sigs/sidekick_sigs.ml
+++ src/sigs/sidekick_sigs.ml
@@ -18,10 +18,12 @@ module type HASH = sig
   val hash : t -> int
 end
 
+type 'a printer = Format.formatter -> 'a -> unit
+
 module type PRINT = sig
   type t
 
-  val pp : t CCFormat.printer
+  val pp : t printer
 end
 
 module type EQ_HASH_PRINT = sig
@@ -37,4 +39,46 @@ module type EQ_ORD_HASH_PRINT = sig
   include PRINT with type t := t
 end
 
-type 'a printer = Format.formatter -> 'a -> unit
+module type DYN_BACKTRACKABLE = sig
+  val n_levels : unit -> int
+  (** Number of levels *)
+
+  val push_level : unit -> unit
+  (** Push a backtracking point *)
+
+  val pop_levels : int -> unit
+  (** [pop_levels n] removes [n] levels *)
+end
+
+module type BACKTRACKABLE0 = sig
+  type t
+
+  val n_levels : t -> int
+  (** Number of levels *)
+
+  val push_level : t -> unit
+  (** Push a backtracking point *)
+
+  val pop_levels : t -> int -> unit
+  (** [pop_levels st n] removes [n] levels *)
+end
+
+module type BACKTRACKABLE1 = sig
+  type 'a t
+
+  val n_levels : _ t -> int
+  (** Number of levels *)
+
+  val push_level : _ t -> unit
+  (** Push a backtracking point *)
+
+  val pop_levels : _ t -> int -> unit
+  (** [pop_levels st n] removes [n] levels *)
+end
+
+module type BACKTRACKABLE1_CB = sig
+  include BACKTRACKABLE1
+
+  val pop_levels : 'a t -> int -> f:('a -> unit) -> unit
+  (** [pop_levels st n ~f] removes [n] levels, calling [f] on every removed item *)
+end
--- src/util/Backtrackable_ref.mli
+++ src/util/Backtrackable_ref.mli
@@ -16,14 +16,4 @@ val get : 'a t -> 'a
 val update : 'a t -> ('a -> 'a) -> unit
 (** Update the reference's current content *)
 
-val push_level : _ t -> unit
-(** Push a backtracking level, copying the current value on top of some
-    stack. The [copy] function will be used if it was provided in {!create}. *)
-
-val n_levels : _ t -> int
-(** Number of saved values *)
-
-val pop_levels : _ t -> int -> unit
-(** Pop [n] levels, restoring to the value the reference was storing [n] calls
-    to [push_level] earlier.
-    @raise Invalid_argument if [n] is bigger than [n_levels]. *)
+include Sidekick_sigs.BACKTRACKABLE1 with type 'a t := 'a t
