6f42c060f4763a9b8aa752e71a32951fc985e753
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-08-14 22:32:51 -0400

    perf(util): more inlining

--- src/util/Log.ml
+++ src/util/Log.ml
@@ -1,6 +1,6 @@
 (** {1 Logging functions, real version} *)
 
-let enabled = true (* NOTE: change here for 0-overhead *)
+let enabled = true (* NOTE: change here for 0-overhead? *)
 
 let debug_level_ = ref 0
 let set_debug l = debug_level_ := l
@@ -9,7 +9,7 @@ let debug_fmt_ = ref Format.err_formatter
 let set_debug_out f = debug_fmt_ := f
 
 (* does the printing, inconditionally *)
-let debug_real_ l k =
+let[@inline never] debug_real_ l k =
   k (fun fmt ->
       Format.fprintf !debug_fmt_ "@[<2>@{<Blue>[%d|%.3f]@}@ " l (Sys.time ());
       Format.kfprintf (fun fmt -> Format.fprintf fmt "@]@.") !debug_fmt_ fmt)
