2a0feed32c79c06610e69f9c4dcc9aa3e65863db
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-08-27 20:48:32 -0400

    format

--- src/smt/registry.mli
+++ src/smt/registry.mli
@@ -1,14 +1,12 @@
-
-
 (** Registry to extract values *)
 
-  type t
-  type 'a key
+type t
+type 'a key
 
-  val create_key : unit -> 'a key
-  (** Call this statically, typically at program initialization, for
+val create_key : unit -> 'a key
+(** Call this statically, typically at program initialization, for
       each distinct key. *)
 
-  val create : unit -> t
-  val get : t -> 'a key -> 'a option
-  val set : t -> 'a key -> 'a -> unit
+val create : unit -> t
+val get : t -> 'a key -> 'a option
+val set : t -> 'a key -> 'a -> unit
