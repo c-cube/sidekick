2764882f50befed9481abc77284d34b094dfdc43
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-09-11 14:50:18 -0400

    fix(preprocess): type check was invalid

--- src/smt/preprocess.ml
+++ src/smt/preprocess.ml
@@ -61,7 +61,7 @@ let preprocess_term_ (self : t) acts (t : term) : term =
           (* only accept a box (with possible side effect: new clauses, etc.) *)
           Log.debugf 20 (fun k ->
               k "(@[smt.preprocess.tr@ %a@ :into %a@])" Term.pp t0 Term.pp u);
-          assert (Term.(equal (ty t) (ty u)));
+          assert (Term.(equal (ty t0) (ty u)));
           u
         | None ->
           (* just preprocess subterms *)
