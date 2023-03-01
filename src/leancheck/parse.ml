402e4038cd429ee070513188448766c2d9f657f7
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-02-28 23:28:12 -0500

    wip: feat(leancheck): start binary to check lean proofs

---
+++ src/leancheck/parse.ml
@@ -0,0 +1,50 @@
+module type CALLBACK = Parse_intf.CALLBACK
+
+type callback = (module CALLBACK)
+type input = [ `String of string | `In of in_channel ]
+
+module type IN = sig
+  val next_line : unit -> string option
+end
+
+let in_of_input (i : input) : (module IN) =
+  match i with
+  | `String s ->
+    (module struct
+      let i = ref 0
+
+      let next_line () =
+        if !i = String.length s then
+          None
+        else (
+          match String.index_from s !i '\n' with
+          | exception Not_found ->
+            let r = Some (String.sub s !i (String.length s - !i)) in
+            i := String.length s;
+            r
+          | j ->
+            let r = Some (String.sub s !i (j - !i)) in
+            i := j + 1;
+            r
+        )
+    end)
+  | `In ic ->
+    (module struct
+      let next_line () = try Some (input_line ic) with End_of_file -> None
+    end)
+
+let parse (input : input) (cb : callback) : unit =
+  let (module CB) = cb in
+  let (module IN) = in_of_input input in
+
+  let rec loop () =
+    match IN.next_line () with
+    | None -> ()
+    | Some line ->
+      Printf.eprintf "line %S\n" line;
+
+      (* TODO: cb *)
+      loop ()
+  in
+
+  loop ()
