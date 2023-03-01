c3e77b2ba1e8d2b6516f37901282fbe473d515ac
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-02-28 23:38:04 -0500

    details for leancheck

--- src/leancheck/leancheck.ml
+++ src/leancheck/leancheck.ml
@@ -3,20 +3,31 @@ module T = Sidekick_core_logic.Term
 let ( let@ ) = ( @@ )
 
 let process_files (files : string list) : unit =
+  let start = Mtime_clock.now () in
   let st = T.Store.create ~size:1024 () in
   Log.debugf 1 (fun k ->
       k "(@[process-files %a@])" Fmt.Dump.(list string) files);
+
+  let n_lines = ref 0 in
 
   let proc_file (file : string) : unit =
     let@ ic = CCIO.with_in file in
     Parse.parse (`In ic)
       (module struct
+        let line _ = incr n_lines
         let ns _ _ = ()
         let ni _ _ = ()
       end)
   in
 
   List.iter proc_file files;
+
+  let elapsed =
+    (Mtime.span (Mtime_clock.now ()) start |> Mtime.Span.to_float_ns) /. 1e9
+  in
+  Log.debugf 1 (fun k ->
+      k "number of lines processed: %d in %.4fs (%.2f/s)" !n_lines elapsed
+        (float !n_lines /. elapsed));
   ()
 
 let () =
